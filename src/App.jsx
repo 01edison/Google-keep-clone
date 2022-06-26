0034c1cf5ed43df7382feedb976af52edf7753d3
MGBEOKWERE CHIMDINDU <cmgbeokwere6@gmail.com>
2022-06-26 02:12:44 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import Note from './Note';
+import notes from './notes';
+
+function App(){
+    return(
+        <div>
+            <Header />
+            {notes.map(function(note){
+                return <>
+                    <Note key={note.id} title={note.title} content={note.content}/>
+                </>
+            })}
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
